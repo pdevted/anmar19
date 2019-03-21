6c79581ea33b26e5f1ba6b33380be86c193f6b76
Ted Kim <yenafirst91@gmail.com>
2019-03-21 05:35:38 -0400

    Add a service

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -7,9 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@workshop/material';
 
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import { ProjectsModule } from './projects/projects.module';
-import { CustomersModule } from './customers/customers.module';
 import { AppRoutingModule } from './app-routing.module';
 import { UiLoginModule } from '@workshop/ui-login';
 
--- apps/dashboard/src/app/projects/projects.component.ts
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService, Project } from '@workshop/core-data';
 
 @Component({
   selector: 'app-projects',
@@ -6,39 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-  projects = [
-    {
-      id: '1',
-      title: 'Project One',
-      details: 'This is a sample project',
-      percentComplete: 20,
-      approved: false
-    },
-    {
-      id: '2',
-      title: 'Project Two',
-      details: 'This is a sample project',
-      percentComplete: 40,
-      approved: false
-    },
-    {
-      id: '3',
-      title: 'Project Three',
-      details: 'This is a sample project',
-      percentComplete: 100,
-      approved: true
-    }
-  ];
+  primaryColor = 'red';
+  projects: Project[];
+  selectedProject: Project;
 
-  selectedProject;
+  constructor(private projectsService: ProjectsService) {}
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.getProjects();
+  }
 
   selectProject(project) {
     this.selectedProject = project;
-    console.log('SELECTED PROJECT', project);
+    // console.log('SELECTED PROJECT', project);
+  }
+
+  getProjects() {
+    this.projects = this.projectsService.all();
   }
 
   cancel() {
